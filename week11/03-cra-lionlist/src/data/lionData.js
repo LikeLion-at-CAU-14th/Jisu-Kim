592e4debc3b7c9e993dd136594fcc4937c99754c
jisukim <joyzone2@naver.com>
2026-05-31 15:40:05 +0900

    feat: week11 과제

---
+++ week11/03-cra-lionlist/src/data/lionData.js
@@ -0,0 +1,38 @@
+//lionData.js 내용
+export const lionData = [
+    // 프론트 멤버 10명
+    { id: 1, name: "류수민", role: "프론트엔드" },
+    { id: 2, name: "이준원", role: "프론트엔드" },
+    { id: 3, name: "최기훈", role: "프론트엔드" },
+    { id: 4, name: "최준혁", role: "프론트엔드" },
+    { id: 5, name: "김지수", role: "프론트엔드" },
+    { id: 6, name: "나희경", role: "프론트엔드" },
+    { id: 7, name: "소재희", role: "프론트엔드" },
+    { id: 8, name: "안서현", role: "프론트엔드" },
+    { id: 9, name: "오유민", role: "프론트엔드" },
+    { id: 10, name: "추서연", role: "프론트엔드" },
+
+    // 백엔드 멤버 10명
+    { id: 11, name: "권민찬", role: "백엔드" },
+    { id: 12, name: "김예은", role: "백엔드" },
+    { id: 13, name: "김주연", role: "백엔드" },
+    { id: 14, name: "김지윤", role: "백엔드" },
+    { id: 15, name: "김희선", role: "백엔드" },
+    { id: 16, name: "서유진", role: "백엔드" },
+    { id: 17, name: "안지요", role: "백엔드" },
+    { id: 18, name: "이시형", role: "백엔드" },
+    { id: 19, name: "조상원", role: "백엔드" },
+    { id: 20, name: "차예은", role: "백엔드" },
+
+    // 기획디자인 멤버 9명
+    { id: 21, name: "곽효석", role: "기획디자인" },
+    { id: 22, name: "김기민", role: "기획디자인" },
+    { id: 23, name: "김다인", role: "기획디자인" },
+    { id: 24, name: "김성은", role: "기획디자인" },
+    { id: 25, name: "김지연", role: "기획디자인" },
+    { id: 26, name: "방세현", role: "기획디자인" },
+    { id: 27, name: "오채린", role: "기획디자인" },
+    { id: 28, name: "윤아영", role: "기획디자인" },
+    { id: 29, name: "후영리", role: "기획디자인" }
+
+];
