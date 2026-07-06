f04d5304b27413998ef758a261f1eaa25a9c2d18
jisukim <joyzone2@naver.com>
2026-07-06 15:58:18 +0900

    feat : 회원 정보 입력 예외 처리 추가

--- week13/src/components/home/FormSection.jsx
+++ week13/src/components/home/FormSection.jsx
@@ -31,7 +31,7 @@ const FormSection = () => {
             return;
         }
 
-        if (formData.email === ''){
+        if (formData.email ===''){
             alert("이메일을 입력해주세요.");
             return;
         }
