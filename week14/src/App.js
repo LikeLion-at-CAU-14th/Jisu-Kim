668bfe5d5a7250f62615940c457d467cb20396dc
jisukim <joyzone2@naver.com>
2026-07-09 10:58:46 +0900

    feat : week14 실습 내용 추가

---
+++ week14/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import PrivateRoute from "./auth/PrivateRoute";
+import Mypage from "./pages/Mypage";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>;
+        <Route path = "/signup" element = {<Signup/>}/>;
+        <Route
+          path = "/mypage"
+          element = {
+            <PrivateRoute>
+              <Mypage/>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
